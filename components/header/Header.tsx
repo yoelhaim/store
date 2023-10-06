1ca2c3f4376bbcc4679fc32dbe8a765691c51447
yoelhaim <youssefelhaimer8@gmail.com>
2023-10-07 00:54:01 +0100

    init project store

---
+++ components/header/Header.tsx
@@ -0,0 +1,17 @@
+export default function Header(){
+    return(
+        <header className="header">
+
+            <div className="container">
+                <div className="img1">
+                </div>
+                <div className="img2"></div>
+                <div className="img3"></div>
+                <div className="img4"></div>
+
+            </div>
+
+
+        </header>
+    )
+}
