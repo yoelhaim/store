aa94dd93433a5ea23e07df7f160f0fffbd02b86e
yoelhaim <youssefelhaimer8@gmail.com>
2023-10-09 02:08:11 +0100

    fix

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -103,7 +103,7 @@ export default function Navbar() {
           {/* <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round"className="tabler-icon tabler-icon-menu-2"><path d="M4 6l16 0"></path><path d="M4 12l16 0"></path><path d="M4 18l16 0"></path></svg> */}
         </div>
         <ul>
-          <li>catagories</li>
+          <li><Link href='/categories'>catagories</Link></li>
           <li>product page</li>
           <li onClick={() => setShow(!show)}>
             <i>{data.length && data.length}</i>
