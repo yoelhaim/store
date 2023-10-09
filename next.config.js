146ed728d4557e351ec0bde3e087bfd073dfb247
yoelhaim <youssefelhaimer8@gmail.com>
2023-10-09 15:43:42 +0100

    add domains for read images

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: false,
     images: {
-        domains: ['localhost', 'i.ibb.co', 'i.imgur.com', 'placeimg.com', 'wampi.ru', 'api.escuelajs.co','media.istockphoto.com'],
+        domains: ['localhost', 'i.ibb.co', 'i.imgur.com', 'placeimg.com', 'wampi.ru', 'api.escuelajs.co','media.istockphoto.com','store.youmer.tech', 'youmer.tech'],
       },
 
 }
